ae4e5595a39e64bc2c5ff7b8e215c8ffd2ecaeee
suhaskolhe11 <suhaskolhe1111@gmail.com>
2025-12-16 10:50:51 +0530

    update: helper functions for data transformation

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,7 +1,9 @@
+
 import yaml
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 import os,sys,dill,pickle
+import numpy as np
 
 def read_yaml_file(file_path:str)->dict:
     try:
@@ -19,5 +21,30 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False):
         os.makedirs(os.path.dirname(file_path),exist_ok=True)
         with open(file_path,'w') as file:
             yaml.dump(content,file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def save_numpy_array_data(file_path:str,array:np.array):
+    """
+    Docstring for save_numpy_array_data
+    
+    :param file_path: str location of file to save
+    :type file_path: str
+    :param array: np.array data to save
+    :type array: np.array
+    """
+    try:
+        dir_path =os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file:
+            np.save(file,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def save_object(file_path,obj:object):
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file:
+            pickle.dump(obj=obj,file=file)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
