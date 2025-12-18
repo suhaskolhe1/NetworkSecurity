a714136c3a7eb4b41f8cfff9a24e530140d1d461
suhaskolhe11 <suhaskolhe1111@gmail.com>
2025-12-18 11:15:13 +0530

    update: main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -38,6 +39,10 @@ if __name__=='__main__':
         logging.info("data Transformation Completed")
 
         print(data_transformation_artifact,end="/n")
+
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config,data_transformation_artifact)
+        model_trainer_arifact = model_trainer.initiate_model_trainer()
 
         
     except Exception as e:
