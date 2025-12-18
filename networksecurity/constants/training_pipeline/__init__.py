3821bcacfb8b614824038d3ce58bdfe229bb3625
suhaskolhe11 <suhaskolhe1111@gmail.com>
2025-12-18 11:09:02 +0530

    update: constants releted to model_trainer

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -15,6 +15,9 @@ TRAIN_FILE_NAME="train.csv"
 TEST_FILE_NAME="test.csv"
 SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
 
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 
 """
 Data Ingestion releted Constant start with DATA_INGESTION VAR NAME
@@ -55,3 +58,17 @@ DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 PREPROCESSING_OBJECT_FILE_NAME:str = "preprocessing.pkl"
+
+
+
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+TRAINING_BUCKET_NAME = "netwworksecurity"
